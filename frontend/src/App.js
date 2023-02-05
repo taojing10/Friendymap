24846a9c002e6487218c0dfb3788afee359c9fb4
Sharon Zhou <sharonzhou@sharons-mbp.mynetworksettings.com>
2023-02-05 11:26:56 -0500

    Resolve comments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,21 +3,19 @@ import { useJsApiLoader, GoogleMap, Marker } from '@react-google-maps/api';
 import {
   SecretsManagerClient,
   GetSecretValueCommand,
-} from "@aws-sdk/client-secrets-manager"
-// import { fromIni } from "@aws-sdk/credential-providers"
+} from "@aws-sdk/client-secrets-manager";
 
 const creds = {
   accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.REACT_APP_SECRET_ACCESS_KEY
 };
 
-const secret_name = "Test_Map_API_Key";
+const secret_name = "REACT_APP_GOOGLE_MAP_API";
 
 
 const client = new SecretsManagerClient({
   region: "us-east-1",
-  credentials: creds
-  // credentials: fromIni({profile: 'default'})
+  credentials: creds,
 });
 
 let response;
@@ -26,24 +24,16 @@ try {
   response = await client.send(
     new GetSecretValueCommand({
       SecretId: secret_name,
-      VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+      VersionStage: "AWSCURRENT", 
     })
   );
 } catch (error) {
-  // For a list of exceptions thrown, see
-  // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-  throw error;
+    throw error;
 }
 
 const secret_json = response.SecretString;
 
-// console.log(secret)
-
 const secret = JSON.parse(secret_json);
-
-// console.log(secret2)
-
-// console.log(secret2['GOOGLE_MAP_API'])
 
 const center = { lat: 38.89766, lng: -77.0365 };
 
@@ -51,7 +41,7 @@ const containerStyle = { width: '100%', height: '100vh' };
 
 function App() {
   const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: secret["GOOGLE_MAP_API"]
+    googleMapsApiKey: secret["REACT_APP_GOOGLE_MAP_API"]
   }); 
 
   if (isLoaded) {
