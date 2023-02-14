06a252fba0e68f2e0d3dd8e306725c97835aa57a
Chewysea <119459532+Chewysea@users.noreply.github.com>
2023-02-13 22:57:11 -0500

    updated user location

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,29 +8,25 @@ const mapStyles = {
 
 function App() {
   const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API,}); 
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API,})
 
   const [ currentPosition, setCurrentPosition ] = useState({
-    lat: 38.89766, lng: -77.0365 });
-  
+    lat: 38.89766, lng: -77.0365 })
     const success = position => {
-    console.log(position.coords)
     setCurrentPosition ({lat: position.coords.latitude,
-      lng: position.coords.longitude} );};
+      lng: position.coords.longitude} );}
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(success);
   },[navigator.geolocation])
 
   if (isLoaded) {
-    console.log(currentPosition)
+   
     return (
       <GoogleMap mapContainerStyle={mapStyles} zoom={13}
-      center={currentPosition}>
-      
-          <Marker position={currentPosition} />
-  
+      center={currentPosition}> <Marker position={currentPosition} />
       </GoogleMap>);} 
+      
       
       else {
     return (<></>);}
