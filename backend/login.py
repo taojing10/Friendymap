2b22869de79a2598820bd8f943ebfd2269ea8138
LexiW <119459400+XinnanW@users.noreply.github.com>
2023-02-14 23:25:13 -0800

    Update login.py

--- backend/login.py
+++ backend/login.py
@@ -1,31 +1,55 @@
+import boto3, json
 from flask import *
 from flask_pymongo import PyMongo
+from botocore.exceptions import ClientError
+# todo:getting mongodb auth
 
 app = Flask(__name__)
+# connect to secret manager resource
+client = boto3.client("secretsmanager")
+
+# retrieve secret value
+def get_secret():
+
+    secret_name = "mongodb_test"
+    region_name = "us-west-2"
+
+    # create a secret manager client
+    client = boto3.client('secretsmanager')
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        raise e
+
+    # Decrypts secret using the associated KMS key.
+    secret = get_secret_value_response['SecretString']
+
 app.config["MONGO_URI"] = "mongodb://localhost:*****/database_name"
 mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    return render_template("home page")
+    return ("home page")
 
-#create a collection in Mongodb to store user information (username and password)
+#create a collection in Mongodb to store user information
 users_collection = mongo.db.users
 
 @app.route("/login", methods = ["POST"])
 def login():
-    if request.method == "POST":
-        #get user info from the form
-        username = request.form["username"]
-        password = request.form["password"]
+    #get user info from the form
+    username = request.form["username"]
+    password = request.form["password"]
 
-        #check if both username and password r correct
-        existing_user = users_collection.find_one({"username": username, "password": password})
-        if existing_user:
-            # store the user ID in the session to keep the user logged in
-            session["user_id"] = str(existing_user["_id"])
-            return "Successfully logged in"
-        else:
-            return "Incorrect username or password"
+    #check if both username and password r correct
+    existing_user = users_collection.find_one({"username": username, "password": password})
+    if not existing_user:
+        return ("Incorrect username or password")
 
-    return render_template("login page")
+    #store the user ID in the session to keep the user logged in
+    session["user"] = user
+
+    return ("Successfully logged in")
+
