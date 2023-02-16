a6537b872b5d0991aae8bd745a3dc3cd6d1f576b
LexiW <119459400+XinnanW@users.noreply.github.com>
2023-02-15 18:27:35 -0800

    Update login.py
    Delete unnecessary route
    changed name find_one to validate_password

--- backend/login.py
+++ backend/login.py
@@ -2,7 +2,6 @@ import boto3, json
 from flask import *
 from flask_pymongo import PyMongo
 from botocore.exceptions import ClientError
-# todo:getting mongodb auth
 
 app = Flask(__name__)
 # connect to secret manager resource
@@ -30,10 +29,6 @@ def get_secret():
 app.config["MONGO_URI"] = f"mongodb+srv://{secret_value[testuser]}:{secret_value[testpw]}@{secret_value[192.168.50.1]}/{secret_value[MongoDB]}?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 
-@app.route("/")
-def index():
-    return ("home page")
-
 #create a collection in Mongodb to store user information
 users_collection = mongo.db.users
 
@@ -44,12 +39,12 @@ def login():
     password = request.form["password"]
 
     #check if both username and password r correct
-    existing_user = users_collection.find_one({"username": username, "password": password})
+    existing_user = users_collection.validate_password({"username": username, "password": password})
     if not existing_user:
         return ("Incorrect username or password")
 
     #store the user ID in the session to keep the user logged in
     session["user"] = user
 
-    return ("Successfully logged in")
+    return ("login page")
 
