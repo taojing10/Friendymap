6e36107c7801115d5b760bcd461c6e6efb63d804
Contributors: Villa Li (@villajwl) <119476997+Villajwl@users.noreply.github.com>
2023-03-01 11:26:00 -0500

    Update connectdbpw.py

--- connectdbpw.py
+++ connectdbpw.py
@@ -1,3 +1,1 @@
-CONNECTION_STRING = "mongodb+srv://testuser:testpw@cluster0.gv9cbee.mongodb.net/?retryWrites=true&w=majority"
-AWS_ACCESS_KEY_ID = 'AKIAQNHZQSGPLQO4VPIN'
-AWS_SECRET_ACCESS_KEY = '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'
+ 
