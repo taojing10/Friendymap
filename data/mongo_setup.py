0b6c392ac3ca038d8b8d1cf472dc3e8aa2daba72
Contributors: Villa Li (@villajwl) <119476997+Villajwl@users.noreply.github.com>
2023-03-07 22:42:02 -0500

    Delete mongo_setup.py

--- data/mongo_setup.py
+++
@@ -1,8 +0,0 @@
-#registering connection.
-
-#step1 import mongoengine
-import mongoengine
-
-#set up 'alias' and 'name'
-def global_init():
-    mongoengine.register_connection(alias='core',name='Friendymap_db')
