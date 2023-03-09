8e0217c0ffb37d48ea1860a429a069fc38c832a3
LexiW <119459400+XinnanW@users.noreply.github.com>
2023-03-08 19:45:18 -0800

    Update login.py

--- backend/login.py
+++ backend/login.py
@@ -1,42 +1,12 @@
-import json
 from flask import Flask, request, jsonify
 from flask_pymongo import MongoClient
-import connectdbpw
-import boto3
-import certifi
-import mongo_setup
+import db_connection
 
 app = Flask('login')
 
-AWS_ACCESS_KEY = connectdbpw.AWS_ACCESS_KEY_ID
-AWS_SECRET_KEY = connectdbpw.SECRET_ACCESS_KEY
-
-# Retrieve the MongoDB connection from AWS Secrets Manager
-secret_name = "MongoDB_connection"
-region_name = "us-east-1"
-
-# Create a boto3 session and retrieve the secret value
-session = boto3.session.Session()
-client = session.client(
-    service_name='secretsmanager',
-    region_name=region_name,
-    aws_access_key_id=AWS_ACCESS_KEY,
-    aws_secret_access_key=AWS_SECRET_KEY
-)
-get_secret_value_response = client.get_secret_value(SecretId=secret_name)
-
-if 'SecretString' in get_secret_value_response:
-    secret = get_secret_value_response['SecretString']
-else:
-    secret = json.loads(get_secret_value_response['SecretBinary'])
-
-secrets = json.loads(secret)
-
-# Set up the MongoDB connection
-connection_string = secrets['connection_string']
-client = MongoClient(connection_string, tlsCAFile=certifi.where())
-db = client['Friendymap_db']
-users = db['User']
+mongo_client = db_connection.get_mongo_client()
+db = mongo_client.Friendymap_db
+users = db.user
 
 def validate_password(username, password):
     # Check if the given username exists in the database and if the password is correct.
