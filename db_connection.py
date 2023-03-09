be1a05d54ea5e80079bbfe1dfc963d80c227a802
Contributors: Villa Li (@villajwl) <119476997+Villajwl@users.noreply.github.com>
2023-03-09 17:20:35 -0500

    Update db_connection.py

--- db_connection.py
+++ db_connection.py
@@ -5,41 +5,38 @@ import json
 import pymongo 
 from flask_pymongo import MongoClient
 import certifi
+import data.mongo_setup as mongo_setup
 import connectdbpw
+
 
 app = Flask(__name__)
 
-#use AWS secretmanager to access MongoDB database
-secret_name = "MongoDB_connection"
-region_name = "us-east-1"
+def get_mongo_client():
+  #use AWS secretmanager to access MongoDB database
+  secret_name = "MongoDB_connection"
+  region_name = "us-east-1"
 
-session = boto3.session.Session(
-  aws_access_key_id=connectdbpw.aws_access_key_id,
-  aws_secret_access_key=connectdbpw.aws_secret_access_key
-)
+  session = boto3.session.Session(
+    aws_access_key_id=connectdbpw.aws_access_key_id,
+    aws_secret_access_key=connectdbpw.aws_secret_access_key
+  )
 
-client = session.client(
-    service_name='secretsmanager',
-    region_name=region_name
-)
-try:
-  # call secret manager
-  get_secret_value_response = client.get_secret_value(SecretId=secret_name)
-except ClientError as error:
-  print(error)
-  
-secret = json.loads(get_secret_value_response['SecretString']) 
+  client = session.client(
+      service_name='secretsmanager',
+      region_name=region_name
+  )
+  try:
+    # call secret manager
+    get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+  except ClientError as error:
+    print(error)
+    
+  secret = json.loads(get_secret_value_response['SecretString']) 
 
-connection_string = secret['connection_string']
+  connection_string = secret['connection_string']
 
-try: 
-  client = MongoClient(connection_string, tlsCAFile=certifi.where())
-  print(client.list_database_names())
-except Exception as e: print(e)
-
-
-
-def main():
-  if __name__ == '__main__': 
-  app.run(debug=True) 
-   
+  try: 
+    client = MongoClient(connection_string, tlsCAFile=certifi.where())
+    print(client.list_database_names())
+    print(client.Friendymap_db.user())
+  except Exception as e: print(e)
